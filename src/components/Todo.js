3ed8aabf645c94670b754960ad59404f44316e6c
JMurphy <murph_iraq@yahoo.com>
2019-05-30 13:39:31 -0400

    added edit function to todo list

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,19 +2,42 @@ import React, { Component } from 'react';
 
 export class Todo extends Component {
 
+    state = {
+        showEdit: false
+    };
+
     handleDelete = () => {
         this.props.deleteTodo(this.props.id)
     }
 
+    handleEdit = () => {
+        this.setState({
+            showEdit: !this.state.showEdit
+        });
+    }
+
 
     render() {
-        return (
-            <div>
-                <button>Edit</button>
-                <button onClick={this.handleDelete}>X</button>
-                <li>{this.props.task}</li>
-            </div>
-        )
+        //Shows edit input if the edit todo button is clicked. Else, shows the todo list item
+        let edit;
+        if(this.state.showEdit) {
+            edit = (
+                <div>
+                    <form>
+                        <input type="text"></input>
+                    </form>
+                </div>
+            )
+        } else {
+            edit = (
+                <div>
+                    <button onClick={this.handleEdit}>Edit</button>
+                    <button onClick={this.handleDelete}>X</button>
+                    <li>{this.props.task}</li>
+                </div>
+            )
+        }
+        return edit;
     }
 }
 
