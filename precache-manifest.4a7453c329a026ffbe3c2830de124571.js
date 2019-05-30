23883dcc04d0abf16ec3f2b8bc239b4169ea12a3
JMurphy <murph_iraq@yahoo.com>
2019-05-30 18:00:44 -0400

    Updates

---
+++ precache-manifest.4a7453c329a026ffbe3c2830de124571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a5b15c67f78d20f849eec070a62d9b1",
+    "url": "/react-to-do/index.html"
+  },
+  {
+    "revision": "3fe1665a81aaccceb70c",
+    "url": "/react-to-do/static/css/main.c4f5c190.chunk.css"
+  },
+  {
+    "revision": "7facb57c28a4f7d64117",
+    "url": "/react-to-do/static/js/2.0f5cd914.chunk.js"
+  },
+  {
+    "revision": "3fe1665a81aaccceb70c",
+    "url": "/react-to-do/static/js/main.b07a1232.chunk.js"
+  },
+  {
+    "revision": "26b717aa96a48dc90608",
+    "url": "/react-to-do/static/js/runtime~main.8def430e.js"
+  }
+]);
